bdfcd5b15c6259de230ba5e11270057d5817b981
jsemalvarez <jsemalvarez@gmail.com>
2018-11-09 12:34:29 -0300

    se creo una función que devuelve todas las películas

--- scripts/script-db.sql
+++ scripts/script-db.sql
@@ -1,15 +1,20 @@
+/* 
+	GUIA 	1
+*/
 CREATE DATABASE QueVeoHoy;
 
 USE QueVeoHoy;
 
-CREATE TABLE pelicula(
-id int NOT NULL,
-titulo varchar(100) NOT NULL,
-duracion int(5),
-director varchar(400),
-fecha_lanzamiento date,
-puntuacion int(2),
-poster varchar(300),
-trama varchar(700),
-PRIMARY KEY(id)
+CREATE TABLE peliculas(
+	id int NOT NULL,
+	titulo varchar(100) NOT NULL,
+	duracion int(5),
+	director varchar(400),
+	anio int(5),
+	fecha_lanzamiento date,
+	puntuacion int(2),
+	poster varchar(300),
+	trama varchar(700),
+	PRIMARY KEY(id)
 ) 
+
---
+++ servidor/controladores/peliculasControlador.js
@@ -0,0 +1,22 @@
+var con = require('../lib/conexionbd.js');
+
+
+function listarPeliculas(req, res){
+	
+	var sql = "SELECT * FROM peliculas";
+	con.query(sql, function(err, result){
+		if(err){
+			console.log("Error en la consulta", err.message);
+			return res.status(404).send("Error en la consulta");
+		}
+		var resultado = {
+			"peliculas": result,
+			"total": result.length
+		}
+			
+		res.status(200).send(JSON.stringify(resultado));		
+	})
+
+}
+
+module.exports.listarPeliculas = listarPeliculas;
