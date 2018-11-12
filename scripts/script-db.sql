aa1348941a321ce16b67a31181d2f7a039683380
jsemalvarez <jsemalvarez@gmail.com>
2018-11-12 15:09:51 -0300

    se crean las tablas actor y actor_pelicula

--- scripts/script-db.sql
+++ scripts/script-db.sql
@@ -16,7 +16,7 @@ CREATE TABLE pelicula(
 	poster varchar(300),
 	trama varchar(700),
 	PRIMARY KEY(id)
-) 
+); 
 
 /* 
 	GUIA 2
@@ -26,12 +26,44 @@ CREATE TABLE genero(
 	id int NOT NULL,
 	nombre varchar(30) NOT NULL,
 	PRIMARY KEY(id)
-)
+);
 
 ALTER TABLE pelicula ADD genero_id int AFTER director;
 
+/* 
+	FIN DEL PASO GUIA 2
+*/
+
+/* 
+	GUIA 3
+*/
+CREATE TABLE actor(
+	id int NOT NULL,
+	nombre varchar(70) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE actor_pelicula(
+	id int NOT NULL,
+	actor_id int NOT NULL,
+	pelicula_id int NOT NULL,
+	PRIMARY KEY(id)
+);
+/* 
+	FIN DEL PASO GUIA 3
+*/
+
 /*
 primero cargar los up date del paso-2 y despues agregar el FK, sino da error
+o crear todas la tablas y refenias primero y despues los update
+*/
+
+/* 
+	GUIA 2
 */
 
 ALTER TABLE pelicula ADD CONSTRAINT FK_generoId_peliculas_R_id_generos FOREIGN KEY (genero_id) REFERENCES genero(id);
+
+/* 
+	FIN DEL PASO GUIA 2
+*/
