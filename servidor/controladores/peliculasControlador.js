6fcdbc40c6e7e141b76622ed32a8c8532237ab55
jsemalvarez <jsemalvarez@gmail.com>
2018-11-12 15:08:09 -0300

    mas funcionalidades a listarPeliculas()

--- servidor/controladores/peliculasControlador.js
+++ servidor/controladores/peliculasControlador.js
@@ -2,8 +2,10 @@ var con = require('../lib/conexionbd.js');
 
 
 function listarPeliculas(req, res){
-	
-	var sql = "SELECT * FROM pelicula";
+	console.log(req.query);
+	console.log(req.query.titulo);
+	var sql = `SELECT * FROM pelicula WHERE  ${valorExist('titulo',req.query.titulo)} AND ${valorExist('genero_id',req.query.genero)} AND ${valorExist('anio',req.query.anio)} ORDER BY ${req.query.columna_orden} ${req.query.tipo_orden} ${limit(req.query.pagina,req.query.cantidad)}`;
+	console.log(sql);		   
 	con.query(sql, function(err, result){
 		if(err){
 			console.log("Error en la consulta", err.message);
@@ -17,6 +19,23 @@ function listarPeliculas(req, res){
 		res.status(200).send(JSON.stringify(resultado));		
 	})
 
+}
+
+function valorExist(clave, valor){
+	if(valor == undefined || valor == null){
+		return 1;
+	}else{
+		if(clave === "titulo"){
+			return clave + " LIKE " +"'%"+ valor +"%'";
+		}else{
+			return clave + "=" + valor;
+		}
+		
+	}
+}
+
+function limit(pagina, cantidad){	
+	return "LIMIT " + (pagina -1) * 52 + "," + cantidad; 
 }
 
 module.exports.listarPeliculas = listarPeliculas;
